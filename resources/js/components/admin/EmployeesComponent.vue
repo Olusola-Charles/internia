d8f7b0907a8231cea80a7d8edeffbdfe44f882d2
Olusola-Charles <64751824+Olusola-Charles@users.noreply.github.com>
2020-08-30 17:46:00 +0100

    Update the Employee tab

--- resources/js/components/admin/EmployeesComponent.vue
+++ resources/js/components/admin/EmployeesComponent.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="container">
         <div class="row mt-5">
-          <div class="col-md-12">
+          <div class="col-md-9">
             <div class="card">
               <div class="card-header">
 
